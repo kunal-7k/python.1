d0155013fca84246eeca37fe0edffdc2ca8db493
kunal kumar <kunalsingh98776kh@gmail.com>
2026-09-01 22:25:58 +0530

    Add files via upload

---
+++ #dictionary related.py
@@ -0,0 +1,10 @@
+#dictionary related
+#list store data or we can say it  can say it hold multiple data in specific order.
+#list use [ brcket to store data and we can store different data in list 
+# and data will be separated by comma,]
+#let's learn to create list
+fruit=["mango","banana","orange"]# these all are list created with items and now let,s print
+print(fruit[0])
+print(fruit[2]) 
+print(fruit[0:2])
+fruit[0]="grape"
