9b78864c8368f4d444756a5e8df1cc354389bae8
kunal kumar <kunalsingh98776kh@gmail.com>
2026-09-01 22:15:29 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,122 @@
+#print("hello kunal")
+#print("i am learning ai")
+# name  = input("enter ur name:")
+# print("welcome")
+# first="kunal"
+# last="singh"
+# print(first +" " +  last)
+# print(5 + 5)
+# name = "kunal"
+# surename="singh"
+# print (name,surename)
+# string ="my name is kunal"
+# print(string)
+# firstname  ="kunal"
+# lastname="singh"
+# print(firstname,lastname)
+# age=25
+# haslisence=False
+# candrive=age>=18 and haslisence
+# print (candrive) 
+# name="mahi"
+# string=f"hi there,my name is {name}"
+# print(string)
+# score=69
+# age=12
+# if score>65 and age<19:
+    # print("he is over qualified")
+# elif score >65:
+    # print("he is qualified")
+# else:print("he is disqualified")
+
+
+# city='mumbai'
+# print(city)
+
+
+# def location(cityname):
+#     print(cityname)
+#     #cityname="mumbai"# this will not work bcz we have to defined it outside the function
+    
+    
+# cityname="mumbai"#here it will work bcz it is defined outside the function
+
+# location(cityname)   
+
+
+
+# def hello(name):
+#     print(name)
+
+# name = "Kunal"
+
+# hello(name)
+
+
+# sum=input()
+# input("what is your name")
+
+
+# test_score = 78
+
+# if test_score >= 95:
+#     print("Grade: A")
+# elif test_score >= 90:
+#     print("Grade: B")
+# elif test_score >= 85:
+#     print("Grade: C")
+# elif test_score >= 80:
+#     print("Grade: D")
+# elif test_score >= 70:
+#     print("Grade: E")
+# else
+# print("Grade: F (Needs Improvement)")
+# range(5) generates numbers from 0 to 4
+# for i in range(5):
+#     print(f"This is repetition number: {i}")
+    # range(start, stop)
+for count in range(1, 4):
+    print(f"Counting... {count}")
+
+print("Ready for data processing!")
+
+
+
+battery = 3
+
+while battery > 0:
+    print(f"System running... Battery at {battery}%")
+    battery -= 1  # Decrease battery by 1 each time
+
+print("System shutting down.") 
+
+
+# for n in range(1, 10):
+    # The modulo operator (%) checks for a remainder
+    # if n % 2 == 0:
+    #     print(f"{n} is an even number.")
+    # If the condition is False, Python simply moves to the next number
+    
+    
+    # Creating a list of strings
+regions = ["North", "South", "East", "West"]
+print(regions)
+
+# Creating a list of integers
+sales_counts = [150, 200, 120, 310]
+print(sales_counts)
+
+# An empty list (useful for filling later)
+new_data = []
+print(new_data)
+
+
+
+# A simple dictionary storing a product's details
+product = {
+    "name": "Laptop",
+    "price": 999.99,
+    "in_stock": True
+}
+
+print(product)
