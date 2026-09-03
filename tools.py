e1dcc4957f6f2ac95b92d94011e905059d96fd5f
kunal kumar <kunalsingh98776kh@gmail.com>
2026-09-03 17:00:38 +0530

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,14 @@
+# import math
+# print(math.sqrt(25))
+
+# from math import sqrt,pi
+# print(sqrt(81))
+
+# from math import sin,cos,radian
+# print(sin(radians(90)))
+
+# import random
+# print(random.randint(1,10))
+# print(random.choice(["banana","grapes","mango"]))   #donn't forget tto lkst the item in []and since u want to print them from list so put then in double quote.
+
+# import pandas as pd
