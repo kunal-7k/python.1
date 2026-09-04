a67f5866912c5e376cbc6d7b6fe9af55df12e19e
kunal kumar <kunalsingh98776kh@gmail.com>
2026-09-04 23:26:09 +0530

    Add files via upload

---
+++ weather_app.py
@@ -0,0 +1,69 @@
+# def location(cityname):
+#     print(cityname)
+# city = input("Enter city: ")
+# location(city)    ``
+
+
+# import requests
+
+# city = input("Enter city name: ")
+
+# url = f"https://wttr.in/{city}?format=j1"
+
+# response = requests.get(url)
+
+# data = response.json()
+
+# temperature = data["current_condition"][0]["temp_C"]
+# humidity = data["current_condition"][0]["humidity"]
+# weather = data["current_condition"][0]["weatherDesc"][0]["value"]
+
+# print("\nWeather Report")
+# print(f"City: {city}")
+# print(f"Temperature: {temperature}°C")
+# print(f"Humidity: {humidity}%")
+# print(f"Condition: {weather}")
+# for i in range(5):
+#     print(i)
+
+# i=1
+# while i<11 :
+#     print(i)
+#     i=i+1
+
+# i=10
+# while i>0 :
+#     print(i)
+#     i=i-1    
+
+
+# for i in range (1,20):
+#     if i%2==0:
+#         print(f"even number: {i}")
+ 
+ 
+# i=2
+# while i<21:
+#     if i%2==0:
+#         print(f"even number: {i}")
+#     i=i+1
+
+# i=1
+# sum=0
+# while i<=10:
+#     sum=sum+i
+#     i=i+1
+    
+# print(sum)
+# sum=0
+# for i in range (1,11):
+    
+#     sum=sum+i
+    
+    
+# print(sum)    
+multiply=1
+for i in range(5,0,-1):
+    multiply=multiply*i
+print(multiply)    
+    
